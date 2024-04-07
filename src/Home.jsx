1816a67969e1da7473aae5f4258cb75d0f73f0cc
ibrahembero <ih1004569bero@gmail.com>
2024-04-07 15:45:20 +0300

    Initial commit (React project)

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+
+
+function Home(){
+   
+    return (
+        <div>
+                
+                <div className='main'>
+                    <div className='content'>
+                         <h1>Home Page</h1>
+                    </div>
+                </div>
+        </div>
+        
+    )
+}
+
+export default Home;
---
+++ src/ShowQuestion.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+
+
+function ShowQuestion(){
+
+    const {id} = useParams();
+    const navigate = useNavigate();
+    const apiUrl = 'http://localhost/subscriber-company/project-one/public/api';
+    const endPoint = '/questions/';
+    const [questionTitle , setQuestionTitle] = useState('')
+    let title =''
+    const [questionImage , setQuestionImage] = useState('')
+    let image =''
+    const getQuestion = async()=>{
+        await axios.get(apiUrl+endPoint+id,
+            {
+                headers: {
+                "Accept" : "application/json",
+                "Content-Type" : "application/json"
+                }
+            }).then(res => {
+                // setQuestion(res.data.question)
+                 console.log(res.data.question)
+                 console.log(res.data.question.question_title)
+                 title = res.data.question.question_title;
+                 setQuestionTitle(title)
+                 console.log(title)
+                 console.log(res.data.question.image)
+                 image = res.data.question.image;
+                 setQuestionImage(image)
+                 console.log(image)
+                 
+            })
+            .catch(err => console.log(err));
+    }
+    useEffect(()=>{
+       getQuestion();
+    },[])
+   
+    return (
+        <div>
+                
+                <div className='main'>
+                    <div className='content'>
+                         <h1>Show Question Page</h1>
+                         <h1>{questionTitle}</h1>
+                         <img src={`http://localhost/subscriber-company/project-one/storage/app/public/${questionImage}`} className='img-design' alt='there is no photo'/>
+                         {/* <img src={'http://127.0.0.1:8000/storage/'+questionImage} className='img-design' alt='there is no photo'/> */}
+                            
+                    </div>
+                </div>
+        </div>
+        
+    )
+}
+
+export default ShowQuestion;
